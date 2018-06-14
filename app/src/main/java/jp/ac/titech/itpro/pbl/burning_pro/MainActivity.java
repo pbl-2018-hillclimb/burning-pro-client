2e212c4c640b30561d0567fe451cd96b42abe812
Arimichi Matsumura <12750219+popon-01@users.noreply.github.com>
2018-06-14 19:54:31 +0900

    Add TwitterManager

--- app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/MainActivity.java
@@ -2,12 +2,22 @@ package jp.ac.titech.itpro.pbl.burning_pro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TwitterManager twitterManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        twitterManager = new TwitterManager(this);
     }
+
+
+    public void tweetTest(View v) {
+    	twitterManager.openTwitter("楽しい！ 人生！");
+	}
 }
