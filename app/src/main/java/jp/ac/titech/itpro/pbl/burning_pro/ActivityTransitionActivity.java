187f0a5babe5f87aa4bb15cbb586972f604dc0a6
arkhe634 <arkhe634@gmail.com>
2018-06-18 09:36:01 +0900

    Add simple activity transition

---
+++ app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/ActivityTransitionActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.titech.itpro.pbl.burning_pro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTransitionActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_transition);
+	}
+
+}
