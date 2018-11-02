2dbe5a7c4c9388c9e5f15f130788a3972cc03e3f
高橋 碧 <takahashi-a-at@se.cs.titech.ac.jp>
2018-11-02 11:30:02 +0900

    Refactor for review

--- app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/RegistrationActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/RegistrationActivity.java
@@ -1,7 +1,6 @@
 package jp.ac.titech.itpro.pbl.burning_pro;
 
 import android.content.Context;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -80,29 +79,8 @@ public class RegistrationActivity extends AppCompatActivity {
         alertDialog.setPositiveButton("送信する", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                // *タイトル，*発言，*発言者，元ネタURL，*元ネタ削除済み？の取得
-                String title = inputTitle.getText().toString();
-                String phrase = inputPhrase.getText().toString();
-                String person = inputPerson.getText().toString();
-                String url = inputURL.getText().toString();
-                Boolean deleted = deleted_yes.isChecked();
-                // 元ネタ投稿日時の取得（※日付のフォーマットが統一されたら以下を実装）
-                // 現状，元ネタ投稿日時は""のみを送信
-                String date = "";
-                if (checkable.isChecked()) {
-                    /*
-                    Calendar date_c = Calendar.getInstance();
-                    date_c.set(Calendar.YEAR, inputDate.getYear());
-                    date_c.set(Calendar.MONTH, inputDate.getMonth());
-                    date_c.set(Calendar.DAY_OF_MONTH, inputDate.getDayOfMonth());
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-                    date = sdf.format(date_c.getTime());
-                    */
-                }
-                // タグの取得
-                String tags = inputTag.getText().toString();
-                // 取得したデータのJSONを作成
-                JSONObject sentJSON = createJSON(title, phrase, person, url, deleted, date, tags);
+                // UIデータのJSONを作成
+                JSONObject sentJSON = createJSON();
                 // JSONをサーバーにPOSTリクエストで送信
                 new PostRequestTask().execute(sentJSON);
                 // ページ遷移
@@ -134,25 +112,30 @@ public class RegistrationActivity extends AppCompatActivity {
 
     /**
      * サーバに送るJSONを作成
-     * @param title *タイトル
-     * @param phrase *発言
-     * @param person *発言者
-     * @param url 元ネタURL
-     * @param deleted *元ネタ削除済み？
-     * @param date 元ネタ投稿日時
-     * @param tags タグ
      */
-    public JSONObject createJSON(String title, String phrase, String person, String url,
-                           Boolean deleted, String date, String tags){
+    public JSONObject createJSON(){
         JSONObject json = new JSONObject();
         try {
-            json.put("title", title);
-            json.put("phrase", phrase);
-            json.put("person", person);
-            json.put("url", url);
-            json.put("deleted", deleted);
+            json.put("title", inputTitle.getText().toString());
+            json.put("phrase", inputPhrase.getText().toString());
+            json.put("person", inputPerson.getText().toString());
+            json.put("url", inputURL.getText().toString());
+            json.put("deleted", deleted_yes.isChecked());
+            // 元ネタ投稿日時の取得（※日付のフォーマットが統一されたら以下を実装）
+            // 現状，元ネタ投稿日時は""のみを送信
+            String date = "";
+            if (checkable.isChecked()) {
+                /*
+                Calendar date_c = Calendar.getInstance();
+                date_c.set(Calendar.YEAR, inputDate.getYear());
+                date_c.set(Calendar.MONTH, inputDate.getMonth());
+                date_c.set(Calendar.DAY_OF_MONTH, inputDate.getDayOfMonth());
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+                date = sdf.format(date_c.getTime());
+                */
+            }
             json.put("published_at", date);
-            json.put("tags", tags);
+            json.put("tags", inputTag.getText().toString());
         } catch (Exception e){
             e.printStackTrace();
         }
