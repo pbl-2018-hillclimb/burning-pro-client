128459f44a6156fe1a860b6c5a6109e169ceecfc
Arimichi Matsumura <12750219+popon-01@users.noreply.github.com>
2018-11-12 18:57:12 +0900

    Add refresh button

--- app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/ImprudenceListActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/ImprudenceListActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,12 +68,22 @@ public class ImprudenceListActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.imprudence_list, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         switch (itemId) {
             case android.R.id.home:
                 finish();
                 break;
+            case R.id.action_refresh_phrases:
+                updateList();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
