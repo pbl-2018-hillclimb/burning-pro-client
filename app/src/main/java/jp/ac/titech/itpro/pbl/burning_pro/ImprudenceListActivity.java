36c3d916c3ed6efbc2b0c3c458f0d633e677e250
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-11-14 23:38:06 +0900

    Show good phrases grouped by person names
    Android Studio による reformat もかけてあるので、一部コメントや
    空白なども変化した。

--- app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/ImprudenceListActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/pbl/burning_pro/ImprudenceListActivity.java
@@ -14,9 +14,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,13 +26,18 @@ import org.json.JSONObject;
 import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-/** 不謹慎発言の一覧を表示するActivityのクラス。 */
+/**
+ * 不謹慎発言の一覧を表示するActivityのクラス。
+ */
 public class ImprudenceListActivity extends AppCompatActivity {
-    private ArrayList<PhraseEntry> phraseList;
-    private ArrayAdapter<PhraseEntry> listAdapter;
+    private HashMap<String, ArrayList<PhraseEntry>> phraseList;
+    private ImprudenceListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,26 +47,26 @@ public class ImprudenceListActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        phraseList = new ArrayList<>();
+        phraseList = new HashMap<>();
 
-        ListView listView = findViewById(R.id.imprudence_list_view);
-        listAdapter =
-            new ImprudenceListAdapter(this, 0, new ArrayList<PhraseEntry>());
+        ExpandableListView listView = findViewById(R.id.imprudence_list_view);
+        listAdapter = new ImprudenceListAdapter(this);
         listView.setAdapter(listAdapter);
 
-        // ListViewのタッチイベントのListener
-        AdapterView.OnItemClickListener clickListener =
-            new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-                    PhraseEntry imprudence = phraseList.get(pos);
-                    String phraseBody = imprudence.phrase.phrase;
-                    Intent intent = new Intent(getApplication(), ImprudentTweetActivity.class);
-                    intent.putExtra("phrase", phraseBody);
-                    startActivity(intent);
-                }
-            };
-        listView.setOnItemClickListener(clickListener);
+        // ExpandableListView の子要素 (サブアイテム) のタッチイベントの Listener
+        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                ImprudenceListAdapter adapter = (ImprudenceListAdapter) parent.getExpandableListAdapter();
+                PhraseEntry entry = adapter.getChild(groupPosition, childPosition);
+                String phraseBody = entry.phrase.phrase;
+                Intent intent = new Intent(getApplication(), ImprudentTweetActivity.class);
+                intent.putExtra("phrase", phraseBody);
+                startActivity(intent);
+                // The click would be always handled.
+                return true;
+            }
+        });
 
         updateList();
     }
@@ -88,44 +92,146 @@ public class ImprudenceListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** 不謹慎発言のリストの更新を行う。 */
-    public void updateList(){
+    /**
+     * 不謹慎発言のリストの更新を行う。
+     */
+    public void updateList() {
         new RequestTask(this).execute();
     }
 
-    /** 取得したJSONからViewの更新を行う。 */
-    protected void refreshView(JSONArray res){
+    /**
+     * 取得したJSONからViewの更新を行う。
+     */
+    protected void refreshView(JSONArray res) {
         phraseList.clear();
         listAdapter.clear();
         try {
             for (int i = 0; i < res.length(); ++i) {
                 JSONObject obj = res.getJSONObject(i);
-                phraseList.add(new PhraseEntry(obj));
+                String displayName = obj.getJSONObject("person").getString("display_name");
+                if (!phraseList.containsKey(displayName)) {
+                    phraseList.put(displayName, new ArrayList<PhraseEntry>());
+                }
+                List entries = phraseList.get(displayName);
+                entries.add(new PhraseEntry(obj));
             }
         } catch (JSONException e) {
             e.printStackTrace();
             phraseList.clear();
         }
-        listAdapter.addAll(phraseList);
+        listAdapter.setEntries(phraseList);
         listAdapter.notifyDataSetChanged();
     }
 
-    /** ListViewの要素を操作するAdapter。 */
-    private static class ImprudenceListAdapter extends ArrayAdapter<PhraseEntry> {
+    /**
+     * ExpandableListViewの要素を操作するAdapter。
+     */
+    private static class ImprudenceListAdapter extends BaseExpandableListAdapter {
+        private List<String> groups;
+        private List<List<PhraseEntry>> children;
+        private Context context = null;
 
-        ImprudenceListAdapter
-            (@NonNull Context context, int resource, @NonNull List<PhraseEntry> objects) {
-            super(context, resource, objects);
+        /**
+         * Constructor
+         */
+        public ImprudenceListAdapter(@NonNull Context ctx) {
+            this.groups = new ArrayList<>();
+            this.children = new ArrayList<>();
+            this.context = ctx;
         }
 
-        @Override @NonNull
-        public View getView(int pos, @Nullable View view, @NonNull ViewGroup parent) {
+        public ImprudenceListAdapter(@NonNull Context ctx, @NonNull List<String> groups, @NonNull List<List<PhraseEntry>> children) {
+            this.groups = groups;
+            this.children = children;
+            this.context = ctx;
+        }
+
+        public void clear() {
+            groups.clear();
+            children.clear();
+        }
+
+        public void setEntries(@NonNull HashMap<String, ArrayList<PhraseEntry>> entries) {
+            clear();
+
+            List<String> groups = new ArrayList<>();
+            groups.addAll(entries.keySet());
+            groups.sort(null);
+            this.groups.addAll(groups);
+            for (String groupName : groups) {
+                this.children.add(entries.get(groupName));
+            }
+        }
+
+        @Override
+        public PhraseEntry getChild(int groupPosition, int childPosition) {
+            return children.get(groupPosition).get(childPosition);
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return children.get(groupPosition).size();
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public String getGroup(int groupPosition) {
+            return groups.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return groups.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        @NonNull
+        public View getGroupView(int groupPosition, boolean isExpanded, View view, ViewGroup parent) {
             if (view == null) {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
+                LayoutInflater inflater = LayoutInflater.from(context);
                 view = inflater.inflate(android.R.layout.simple_list_item_2,
                     parent, false);
             }
-            PhraseEntry imprudence = getItem(pos);
+            String groupName = getGroup(groupPosition);
+            if (groupName != null) {
+                TextView personText = view.findViewById(android.R.id.text1);
+                personText.setText(groupName);
+
+                int numPhrases = getChildrenCount(groupPosition);
+                TextView countText = view.findViewById(android.R.id.text2);
+                countText.setText(numPhrases + " 件の発言");
+            }
+            return view;
+        }
+
+        @Override
+        @NonNull
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View view, ViewGroup parent) {
+            if (view == null) {
+                LayoutInflater inflater = LayoutInflater.from(context);
+                view = inflater.inflate(android.R.layout.simple_list_item_2,
+                    parent, false);
+            }
+            PhraseEntry imprudence = getChild(groupPosition, childPosition);
             if (imprudence != null) {
                 TextView titleText = view.findViewById(android.R.id.text1);
                 titleText.setText(imprudence.phrase.title);
@@ -137,7 +243,9 @@ public class ImprudenceListActivity extends AppCompatActivity {
         }
     }
 
-    /** 通信を行う非同期処理。 */
+    /**
+     * 通信を行う非同期処理。
+     */
     private static class RequestTask extends AsyncTask<Void, Void, Optional<JSONArray>> {
         private WeakReference<ImprudenceListActivity> activityRef;
         private String requestURL;
